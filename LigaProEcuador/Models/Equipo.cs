d9ba2bdd6ff03c5660216b3f073bb8554dadf245
Mateo Mikael Pillajo Guijarro <mateo.pillajo@udla.edu.ec>
2024-10-20 17:12:49 -0500

    Agregar archivos de proyecto.

---
+++ LigaProEcuador/Models/Equipo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LigaProEcuador.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int IdEquipo { get; set; }
+        public string NombreEquipo {  get; set; }
+
+        public string CiudadEquipo {  get; set; }
+        public int Titulos {  get; set; }
+        public string AceptaExtranjeros {  get; set; }
+        public string Estadio {  get; set; }
+
+    }
+}
---
+++ LigaProEcuador/Models/Estadio.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LigaProEcuador.Models
+{
+    public class Estadio
+    {
+        [Key]
+        public int IdEstadio { get; set; }
+        public string Direccion {  get; set; }
+        public string CiudadEstadio { get; set; }
+        public int Capacidad {  get; set; }
+        public Equipo Equipo { get; set; }
+        [ForeignKey("Equipo")]
+        public int IdEquipo { get; set; }
+    }
+}
