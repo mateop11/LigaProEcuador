58ebed5f86e577e02ed27c49431e3c767d2f8845
Mateo Mikael Pillajo Guijarro <mateo.pillajo@udla.edu.ec>
2024-10-20 18:47:25 -0500

    Se desarrollo los controladores, se agrego los diferentes equipos de la Liga Pro y sus estadios, conjunto con sus caracterisitcas.

--- LigaProEcuador/Data/LigaProEcuadorContext.cs
+++ LigaProEcuador/Data/LigaProEcuadorContext.cs
@@ -1,21 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using LigaProEcuador.Models;
 
 namespace LigaProEcuador.Data
 {
     public class LigaProEcuadorContext : DbContext
     {
-        public LigaProEcuadorContext (DbContextOptions<LigaProEcuadorContext> options)
+        public LigaProEcuadorContext(DbContextOptions<LigaProEcuadorContext> options)
             : base(options)
         {
         }
 
-        public DbSet<LigaProEcuador.Models.Equipo> Equipo { get; set; } = default!;
-        public DbSet<LigaProEcuador.Models.Estadio> Estadio { get; set; } = default!;
-        public DbSet<LigaProEcuador.Models.Jugador> Jugador { get; set; } = default!;
+        public DbSet<Equipo> Equipo { get; set; } = default!;
+        public DbSet<Estadio> Estadio { get; set; } = default!;
+        public DbSet<Jugador> Jugador { get; set; } = default!;
+
+        // Seeding de datos
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Seeding de Equipos
+            modelBuilder.Entity<Equipo>().HasData(
+                new Equipo { IdEquipo = 1, NombreEquipo = "Barcelona SC", CiudadEquipo = "Guayaquil", Titulos = 16, AceptaExtranjeros = "si", Estadio = "Estadio Banco Pichincha" },
+                new Equipo { IdEquipo = 2, NombreEquipo = "CS Emelec", CiudadEquipo = "Guayaquil", Titulos = 14, AceptaExtranjeros = "si", Estadio = "Estadio George Capwell Banco del Austro" },
+                new Equipo { IdEquipo = 3, NombreEquipo = "Sociedad Deportiva Aucas", CiudadEquipo = "Quito", Titulos = 1, AceptaExtranjeros = "si", Estadio = "Estadio Cooprogreso Gonzalo Pozo Ripalda" },
+                new Equipo { IdEquipo = 4, NombreEquipo = "Liga Deportiva Universitaria", CiudadEquipo = "Quito", Titulos = 12, AceptaExtranjeros = "si", Estadio = "Estadio Rodrigo Paz Delgado" },
+                new Equipo { IdEquipo = 5, NombreEquipo = "Club Deportivo El Nacional", CiudadEquipo = "Quito", Titulos = 13, AceptaExtranjeros = "si", Estadio = "Estadio Olímpico Atahualpa" },
+                new Equipo { IdEquipo = 6, NombreEquipo = "Delfín Sporting Club", CiudadEquipo = "Manta", Titulos = 1, AceptaExtranjeros = "si", Estadio = "Estadio Jocay" },
+                new Equipo { IdEquipo = 7, NombreEquipo = "Club Deportivo Cuenca", CiudadEquipo = "Cuenca", Titulos = 1, AceptaExtranjeros = "si", Estadio = "Banco del Austro Alejandro Serrano Aguilar" },
+                new Equipo { IdEquipo = 8, NombreEquipo = "Club Universidad Católica", CiudadEquipo = "Quito", Titulos = 0, AceptaExtranjeros = "si", Estadio = "Estadio Olímpico Atahualpa" },
+                new Equipo { IdEquipo = 9, NombreEquipo = "Club Deportivo Macará", CiudadEquipo = "Ambato", Titulos = 0, AceptaExtranjeros = "si", Estadio = "Estadio Universidad Indoamérica Bellavista" },
+                new Equipo { IdEquipo = 10, NombreEquipo = "Club Independiente del Valle", CiudadEquipo = "Quito", Titulos = 6, AceptaExtranjeros = "si", Estadio = "Estadio Banco Guayaquil" },
+                new Equipo { IdEquipo = 11, NombreEquipo = "Club Técnico Universitario", CiudadEquipo = "Ambato", Titulos = 0, AceptaExtranjeros = "si", Estadio = "Estadio Universidad Indoamérica Bellavista" },
+                new Equipo { IdEquipo = 12, NombreEquipo = "Orense Sporting Club", CiudadEquipo = "Machala", Titulos = 0, AceptaExtranjeros = "si", Estadio = "Estadio 9 de mayo" },
+                new Equipo { IdEquipo = 13, NombreEquipo = "Cumbayá Fútbol Club", CiudadEquipo = "Quito", Titulos = 0, AceptaExtranjeros = "si", Estadio = "Estadio Olímpico Atahualpa" },
+                new Equipo { IdEquipo = 14, NombreEquipo = "Mushuc Runa", CiudadEquipo = "Ambato", Titulos = 0, AceptaExtranjeros = "si", Estadio = "Cooperativa de Ahorro y Crédito Mushuc Runa" },
+                new Equipo { IdEquipo = 15, NombreEquipo = "Imbabura", CiudadEquipo = "Ibarra", Titulos = 0, AceptaExtranjeros = "si", Estadio = "Estadio Olímpico de Ibarra" },
+                new Equipo { IdEquipo = 16, NombreEquipo = "Libertad Fútbol Club", CiudadEquipo = "Loja", Titulos = 0, AceptaExtranjeros = "si", Estadio = "Estadio Reina del Cisne" }
+            );
+
+            // Seeding de Estadios
+            modelBuilder.Entity<Estadio>().HasData(
+                new Estadio { IdEstadio = 1, Direccion = "Av. Barcelona", CiudadEstadio = "Guayaquil", Capacidad = 59283, IdEquipo = 1 },
+                new Estadio { IdEstadio = 2, Direccion = "Gral. José A.Gómez", CiudadEstadio = "Guayaquil", Capacidad = 40000, IdEquipo = 2 },
+                new Estadio { IdEstadio = 3, Direccion = "Av. Rumichaca Ñan", CiudadEstadio = "Quito", Capacidad = 18799, IdEquipo = 3 },
+                new Estadio { IdEstadio = 4, Direccion = "Av. John F. Kennedy 817", CiudadEstadio = "Quito", Capacidad = 41575, IdEquipo = 4 },
+                new Estadio { IdEstadio = 5, Direccion = "Av. Naciones Unidas", CiudadEstadio = "Quito", Capacidad = 38258, IdEquipo = 5 },
+                new Estadio { IdEstadio = 6, Direccion = "27PW+FRW", CiudadEstadio = "Manta", Capacidad = 22000, IdEquipo = 6 },
+                new Estadio { IdEstadio = 7, Direccion = "Av. del Estadio", CiudadEstadio = "Cuenca", Capacidad = 18549, IdEquipo = 7 },
+                new Estadio { IdEstadio = 8, Direccion = "Av. Naciones Unidas", CiudadEstadio = "Quito", Capacidad = 38258, IdEquipo = 8 },
+                new Estadio { IdEstadio = 9, Direccion = "Av Bolivariana", CiudadEstadio = "Ambato", Capacidad = 16467, IdEquipo = 9 },
+                new Estadio { IdEstadio = 10, Direccion = "Complejo Independiente del Valle", CiudadEstadio = "Quito", Capacidad = 12000, IdEquipo = 10 },
+                new Estadio { IdEstadio = 11, Direccion = "Av Bolivariana", CiudadEstadio = "Ambato", Capacidad = 16467, IdEquipo = 11 },
+                new Estadio { IdEstadio = 12, Direccion = "Avenida de Las Palmeras 919", CiudadEstadio = "Machala", Capacidad = 16500, IdEquipo = 12 },
+                new Estadio { IdEstadio = 13, Direccion = "Av. Naciones Unidas", CiudadEstadio = "Quito", Capacidad = 38258, IdEquipo = 13 },
+                new Estadio { IdEstadio = 14, Direccion = "Via Ambato Guaranda, San Alfonso de Chibuleo", CiudadEstadio = "Ambato", Capacidad = 8200, IdEquipo = 14 },
+                new Estadio { IdEstadio = 15, Direccion = "Av. Jaime Roldos Aguilera y, Sánchez y Cifuentes", CiudadEstadio = "Ibarra", Capacidad = 17300, IdEquipo = 15 },
+                new Estadio { IdEstadio = 16, Direccion = "avenida Emiliano Ortega y calle Azuay", CiudadEstadio = "Loja", Capacidad = 14935, IdEquipo = 16 }
+            );
+        }
     }
 }
--- LigaProEcuador/Migrations/20241020233817_Migracion1.cs
+++ LigaProEcuador/Migrations/20241020233817_Migracion1.cs
@@ -2,6 +2,8 @@
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace LigaProEcuador.Migrations
 {
     /// <inheritdoc />
@@ -69,6 +71,52 @@ namespace LigaProEcuador.Migrations
                         principalTable: "Equipo",
                         principalColumn: "IdEquipo",
                         onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Equipo",
+                columns: new[] { "IdEquipo", "AceptaExtranjeros", "CiudadEquipo", "Estadio", "NombreEquipo", "Titulos" },
+                values: new object[,]
+                {
+                    { 1, "si", "Guayaquil", "Estadio Banco Pichincha", "Barcelona SC", 16 },
+                    { 2, "si", "Guayaquil", "Estadio George Capwell Banco del Austro", "CS Emelec", 14 },
+                    { 3, "si", "Quito", "Estadio Cooprogreso Gonzalo Pozo Ripalda", "Sociedad Deportiva Aucas", 1 },
+                    { 4, "si", "Quito", "Estadio Rodrigo Paz Delgado", "Liga Deportiva Universitaria", 12 },
+                    { 5, "si", "Quito", "Estadio Olímpico Atahualpa", "Club Deportivo El Nacional", 13 },
+                    { 6, "si", "Manta", "Estadio Jocay", "Delfín Sporting Club", 1 },
+                    { 7, "si", "Cuenca", "Banco del Austro Alejandro Serrano Aguilar", "Club Deportivo Cuenca", 1 },
+                    { 8, "si", "Quito", "Estadio Olímpico Atahualpa", "Club Universidad Católica", 0 },
+                    { 9, "si", "Ambato", "Estadio Universidad Indoamérica Bellavista", "Club Deportivo Macará", 0 },
+                    { 10, "si", "Quito", "Estadio Banco Guayaquil", "Club Independiente del Valle", 6 },
+                    { 11, "si", "Ambato", "Estadio Universidad Indoamérica Bellavista", "Club Técnico Universitario", 0 },
+                    { 12, "si", "Machala", "Estadio 9 de mayo", "Orense Sporting Club", 0 },
+                    { 13, "si", "Quito", "Estadio Olímpico Atahualpa", "Cumbayá Fútbol Club", 0 },
+                    { 14, "si", "Ambato", "Cooperativa de Ahorro y Crédito Mushuc Runa", "Mushuc Runa", 0 },
+                    { 15, "si", "Ibarra", "Estadio Olímpico de Ibarra", "Imbabura", 0 },
+                    { 16, "si", "Loja", "Estadio Reina del Cisne", "Libertad Fútbol Club", 0 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Estadio",
+                columns: new[] { "IdEstadio", "Capacidad", "CiudadEstadio", "Direccion", "IdEquipo" },
+                values: new object[,]
+                {
+                    { 1, 59283, "Guayaquil", "Av. Barcelona", 1 },
+                    { 2, 40000, "Guayaquil", "Gral. José A.Gómez", 2 },
+                    { 3, 18799, "Quito", "Av. Rumichaca Ñan", 3 },
+                    { 4, 41575, "Quito", "Av. John F. Kennedy 817", 4 },
+                    { 5, 38258, "Quito", "Av. Naciones Unidas", 5 },
+                    { 6, 22000, "Manta", "27PW+FRW", 6 },
+                    { 7, 18549, "Cuenca", "Av. del Estadio", 7 },
+                    { 8, 38258, "Quito", "Av. Naciones Unidas", 8 },
+                    { 9, 16467, "Ambato", "Av Bolivariana", 9 },
+                    { 10, 12000, "Quito", "Complejo Independiente del Valle", 10 },
+                    { 11, 16467, "Ambato", "Av Bolivariana", 11 },
+                    { 12, 16500, "Machala", "Avenida de Las Palmeras 919", 12 },
+                    { 13, 38258, "Quito", "Av. Naciones Unidas", 13 },
+                    { 14, 8200, "Ambato", "Via Ambato Guaranda, San Alfonso de Chibuleo", 14 },
+                    { 15, 17300, "Ibarra", "Av. Jaime Roldos Aguilera y, Sánchez y Cifuentes", 15 },
+                    { 16, 14935, "Loja", "avenida Emiliano Ortega y calle Azuay", 16 }
                 });
 
             migrationBuilder.CreateIndex(
