2c19882ff0aa5d9bb35bfc12520eab32907e7ae7
Mateo Mikael Pillajo Guijarro <mateo.pillajo@udla.edu.ec>
2024-10-20 19:44:12 -0500

    Se termino de arreglar la estetica del proeycto y algunos detalles mas.

--- LigaProEcuador/Program.cs
+++ LigaProEcuador/Program.cs
@@ -1,13 +1,13 @@
-<<<<<<< HEAD
+
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using LigaProEcuador.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<LigaProEcuadorContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("LigaProEcuadorContext") ?? throw new InvalidOperationException("Connection string 'LigaProEcuadorContext' not found.")));
-=======
-var builder = WebApplication.CreateBuilder(args);
->>>>>>> d9ba2bdd6ff03c5660216b3f073bb8554dadf245
+
+
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
